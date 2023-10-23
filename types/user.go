f8923eb2fc5e987c354072972c3aa948aed46b50
Rosa Soto <rsh456@hotmail.com>
2023-10-22 20:37:57 -0600

    add user validations
    - Use regex for validate email
    - Add length characters for first name, last name and password

--- types/user.go
+++ types/user.go
@@ -1,13 +1,19 @@
 package types
 
 import (
+	"fmt"
+	"regexp"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 	_ "golang.org/x/crypto/bcrypt"
 )
 
 const (
-	bcryptCost = 12
+	bcryptCost      = 12
+	minFirstNameLen = 2
+	minLastNameLen  = 2
+	minPasswordLen  = 7
 )
 
 type CreateUserParams struct {
@@ -17,13 +23,35 @@ type CreateUserParams struct {
 	Password  string `json:"password"`
 }
 
+func (params CreateUserParams) Validate() []string {
+	errors := []string{}
+	if len(params.FirstName) < minFirstNameLen {
+		errors = append(errors, fmt.Sprintf("firstName length should be at least %d characters", minFirstNameLen))
+	}
+	if len(params.FirstName) < minLastNameLen {
+		errors = append(errors, fmt.Sprintf("lastName length should be at least %d characters", minLastNameLen))
+	}
+	if len(params.Password) < minPasswordLen {
+		errors = append(errors, fmt.Sprintf("password length should be at least %d characters", minPasswordLen))
+	}
+	if !isEmailValid(params.Email) {
+		errors = append(errors, fmt.Sprintf("email is invalid "))
+	}
+	return errors
+}
+
+func isEmailValid(e string) bool {
+	emailRegex := regexp.MustCompile(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`)
+	return emailRegex.MatchString(e)
+}
+
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	FirstName string             `bson: "firstName" json:"firstName"`
 	LastName  string             `bson: "lastName" json:"lastName"`
 	Email     string             `bson: "email" json:"email"`
 	//we don't want to show encrypted password in json
-	EncryptedPassword string `bson:EncryptedPassword json:"-"`
+	EncryptedPassword string `bson:"EncryptedPassword" json:"-"`
 }
 
 func NewUserFromParams(params CreateUserParams) (*User, error) {
