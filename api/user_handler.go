10b46f1f584fe446929c30d759d9c9be0e3f175f
Rosa Soto <rsh456@hotmail.com>
2023-10-29 19:42:42 -0600

    feat: update user

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/rsh456/reservation-system/db"
 	"github.com/rsh456/reservation-system/types"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserHandler struct {
@@ -16,6 +18,18 @@ func NewUserHandler(userStore db.UserStore) *UserHandler {
 	return &UserHandler{
 		userStore: userStore,
 	}
+}
+
+func (h *UserHandler) HandlePutUser(c *fiber.Ctx) error {
+	return nil
+}
+
+func (h *UserHandler) HandleDeleteUser(c *fiber.Ctx) error {
+	userID := c.Params("id")
+	if err := h.userStore.DeleteUser(c.Context(), userID); err != nil {
+		return err
+	}
+	return c.JSON(map[string]string{"deleted": userID})
 }
 
 func (h *UserHandler) HandlePostUser(c *fiber.Ctx) error {
@@ -45,6 +59,9 @@ func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
 	)
 	user, err := h.userStore.GetUserByID(c.Context(), id)
 	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.JSON(map[string]string{"msg": "not found"})
+		}
 		return err
 	}
 	return c.JSON(user)
