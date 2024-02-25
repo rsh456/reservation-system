315a091861e44b9fc83339dd9d6a3d4ff4f29fc5
Rosa Soto <rsh456@hotmail.com>
2024-02-25 12:24:46 -0600

    - clean db reference from handlers to make it loosely coupled

--- api/hotel_handler.go
+++ api/hotel_handler.go
@@ -45,11 +45,7 @@ func (h *HotelHandler) HandleGetHotels(c *fiber.Ctx) error {
 
 func (h *HotelHandler) HandleGetHotel(c *fiber.Ctx) error {
 	id := c.Params("id")
-	oid, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return ErrInvalidID()
-	}
-	hotel, err := h.store.Hotel.GetHotelByID(c.Context(), oid)
+	hotel, err := h.store.Hotel.GetHotelByID(c.Context(), id)
 	if err != nil {
 		return err
 	}
