fe7601469a26f6fdf9db6502e433d4c207feac83
Rosa Soto <rsh456@hotmail.com>
2025-04-15 10:06:43 -0500

    chore: Use authentication header instead of custom X-Api_Token to prevent CORS validation issues. -Update Dockerfile and Makefile to use port 8502 instead of 5000

--- api/jwt.go
+++ api/jwt.go
@@ -14,10 +14,11 @@ import (
 // declarative pattern by declarating a function into another function
 func JWTAuthentication(userStore db.UserStore) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		token, ok := c.GetReqHeaders()["X-Api-Token"]
+		token, ok := c.GetReqHeaders()["Authorization"]
 		if !ok {
 			return ErrUnAuthorized()
 		}
+		token = token[len("Bearer "):]
 		claims, err := validateToken(token)
 		if err != nil {
 			return err
